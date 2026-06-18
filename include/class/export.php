651410402319af006fcb2af1b4b1968f81c71a2c
Cris Firman <cris@7cups.com>
2026-06-18 17:05:09 -0400

    test

--- include/class/export.php
+++ include/class/export.php
@@ -31,7 +31,7 @@ class export
                 break;
 
             case "pdf":
-                var_dump($data, $this->file_location, $this->file_name);
+                var_dump($this->file_location, $this->file_name);
                 pdfThis($data, $this->file_location, $this->file_name);
                 if ($this->file_location === "download" || $this->file_location === "inline") exit();
                 break;
